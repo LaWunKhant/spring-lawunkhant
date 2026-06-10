6da9f2583ccf8227fdd67ca91c911d65af3b5905
LaWunKhant <lawunkhant.mr3@gmail.com>
2026-06-10 14:04:50 +0900

    chore: first commit spring boot project

---
+++ src/main/java/com/cmps/spring/SpringLawunkhantApplication.java
@@ -0,0 +1,13 @@
+package com.cmps.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLawunkhantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLawunkhantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmps/spring/SpringLawunkhantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmps.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLawunkhantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
