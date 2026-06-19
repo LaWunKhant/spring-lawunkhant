e4e511b18f281262c19911009754464c2cf1f135
LaWunKhant <lawunkhant.mr3@gmail.com>
2026-06-19 16:17:30 +0900

    マイグレーション

---
+++ src/main/java/com/cmps/spring/FlywayConfig.java
@@ -0,0 +1,21 @@
+package com.cmps.spring;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+    
+    @Bean
+    public Flyway flyway(javax.sql.DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
+}
